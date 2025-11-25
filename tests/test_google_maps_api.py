d768ea9ed66265df3c08749c0fd871942848dd62
buladash <bulkadash@gmIail.com>
2025-11-25 15:42:00 +0100

    Setup API testing project

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -9,7 +9,7 @@ from utils.api import Google_maps_api
 class Test_create_place():
     @allure.description("Test create, update, delete new place")
     def test_create_new_place(self):
-        print("Метод Post")
+        print("Method Post")
         result_post = Google_maps_api.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
@@ -19,21 +19,21 @@ class Test_create_place():
         Checking.check_json_value(result_post, 'status', 'OK')
 
 
-        print("Метод Get Post")
+        print("Method Get Post")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
         Checking.check_json_fields(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
         Checking.check_json_value(result_get, 'address', '29, side layout, cohen 09')
 
 
-        print("Метод Put")
+        print("Method Put")
         result_put = Google_maps_api.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
         Checking.check_json_fields(result_put, ['msg'])
         Checking.check_json_value(result_put, 'msg', 'Address successfully updated')
 
 
-        print("Метод Get Put")
+        print("Method Get Put")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
         Checking.check_json_fields(result_get,
@@ -41,13 +41,13 @@ class Test_create_place():
                                     'language'])
         Checking.check_json_value(result_get, 'address', '100 Lenina street, RU')
 
-        print("Метод Delete")
+        print("Method Delete")
         result_delete = Google_maps_api.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
         Checking.check_json_fields(result_delete, ['status'])
         Checking.check_json_value(result_delete, 'status', 'OK')
 
-        print("Метод Get Delete")
+        print("Method Get Delete")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
         Checking.check_json_fields(result_get, ['msg'])
